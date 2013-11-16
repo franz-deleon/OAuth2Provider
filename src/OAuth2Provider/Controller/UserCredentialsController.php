34030e7f1c7431a1e360f68ed9df38ecee11e91e
Franz De Leon <fleo@osidev-imac2.loctest.gov>
2013-11-15 19:35:41 -0500

    restfult controller

--- src/OAuth2Provider/Controller/UserCredentialsController.php
+++ src/OAuth2Provider/Controller/UserCredentialsController.php
@@ -3,8 +3,9 @@ namespace OAuth2Provider\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Mvc\Controller\AbstractRestfulController;
 
-class UserCredentialsController extends AbstractActionController
+class UserCredentialsController extends AbstractRestfulController
 {
     public function indexAction()
     {
@@ -13,9 +14,27 @@ class UserCredentialsController extends AbstractActionController
 
     public function RequestTokenAction()
     {
-        $r = $this->getServiceLocator()->get('oauth2provider.server.main');
-        //$r = $this->getServiceLocator()->get('OAuth/Storage/ClientCredentials')->getClientDetails(123);
-        var_dump($r);
+        $server = $this->getServiceLocator()->get('oauth2provider.server.main');
+// $x = $this->getServiceLocator()->get('request');var_dump($x->getPost('grant_type'));
+        $server->handleTokenRequest(\OAuth2\Request::createFromGlobals())->send();
+        //var_dump($r);
+
+        //$request = \OAuth2\Request::createFromGlobals();
+
+        //$userCred = $this->getServiceLocator()->get('ApiOauth2Server/Storage/UserCredentials')->checkUserCredentials('kelmadics', 'abc123');
+//var_dump($userCred);
+//         $this->getServiceLocator()->get('ApiOauth2Server/Storage/UserCredentials')->getUserDetails('kelmadics');
+
+//         $r = $this->getServiceLocator()->get('ApiOauth2Server/Storage/AccessToken')->getAccessToken('123');
+
+
+
+//         //$this->getServiceLocator()->get('ApiOauth2Server/Storage/AccessToken')->setAccessToken('456', 1, 1, '2014-05-05');
+
+//         $r = $this->getServiceLocator()->get('ApiOauth2Server/Storage/ClientCredentials')->checkClientCredentials(1, 123);
+
+
+
         return new ViewModel();
     }
 }
