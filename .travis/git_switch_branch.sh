479444964d0e45c435f71c46f3860d02a2f9d0f7
fdeleon <kelmadics@gmail.com>
2014-03-11 16:39:27 -0400

    cleaning up travis

--- .travis/git_switch_branch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# author: franz de leon
-# grab the branch and place it in travis composer json
-
-GIT_BRANCH=$(git branch 2>/dev/null| sed -n '/^\*/s/^\* //p')
-sed -i "s/%branch%/${GIT_BRANCH}/g" .travis/travis.composer.json
